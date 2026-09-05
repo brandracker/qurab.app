e9ac9c354c3c6c35cadd70e2076647101b6451eb
brandracker <contact@brandracker.com>
2026-09-05 16:33:43 -0700

    Fix Android build by removing obsolete UnityAdsPlugin and adding Java 17 compileOptions for AdMob

--- serene-union-app/android/app/src/main/java/app/qurb/serene/MainActivity.java
+++ serene-union-app/android/app/src/main/java/app/qurb/serene/MainActivity.java
@@ -7,7 +7,6 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(AdMobPlugin.class);
-        registerPlugin(UnityAdsPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
